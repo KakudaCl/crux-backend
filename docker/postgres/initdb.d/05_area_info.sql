ae015e80d87d5a6a84cd891a8e04fd14a9e76f3c
角田 直樹 <kakuda@iret.co.jp>
2026-01-28 17:14:17 +0900

    fix: 一部テーブルでカラムが不足していたため修正

--- docker/postgres/initdb.d/05_area_info.sql
+++ docker/postgres/initdb.d/05_area_info.sql
@@ -3,13 +3,18 @@
 -- =====================================
 
 CREATE TABLE IF NOT EXISTS area_info (
+    gym_id BIGINT NOT NULL,
     area_id BIGSERIAL,
     area_name VARCHAR(10) NOT NULL,
     CONSTRAINT area_info_pkey PRIMARY KEY (area_id),
+    CONSTRAINT area_info_gym_id_fkey FOREIGN KEY (gym_id)
+        REFERENCES gym_info(gym_id)
+        ON DELETE CASCADE,
     CONSTRAINT area_info_area_id_key UNIQUE (area_id),
     CONSTRAINT area_info_area_name_key UNIQUE (area_name)
 );
 
 COMMENT ON TABLE area_info IS 'エリア情報';
+COMMENT ON COLUMN area_info.gym_id IS 'ジムID';
 COMMENT ON COLUMN area_info.area_id IS 'エリアID';
 COMMENT ON COLUMN area_info.area_name IS 'エリア名';
