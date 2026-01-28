a03de5c020fa41e5c0440b4ccb18d4c4453bbec9
角田 直樹 <kakuda@iret.co.jp>
2026-01-28 18:28:19 +0900

    fix: SQL文末尾のセミコロン漏れを対処

--- docker/postgres/initdb.d/10_master_data.sql
+++ docker/postgres/initdb.d/10_master_data.sql
@@ -4,29 +4,20 @@
 
 -- 都道府県情報の初期データ
 INSERT INTO prefecture_info (prefecture_name) VALUES
-    ('大阪府'), ('滋賀県'), ('東京都')
-ON CONFLICT (prefecture_name) DO NOTHING;
+    ('大阪府'), ('滋賀県'), ('東京都');
 
 -- ジム情報の初期データ
 INSERT INTO gym_info (gym_name, prefecture_id) VALUES
-    ('Dボルダリングなんば', 1), ('クライミングバム大阪店', 1), ('CRUX大阪', 1),('ロックメイト大津店', 2), ('B-PUMP OGIKUBO', 3)
-ON CONFLICT (gym_name) DO NOTHING;
+    ('Dボルダリングなんば', 1), ('クライミングバム大阪店', 1), ('CRUX大阪', 1),('ロックメイト大津店', 2), ('B-PUMP OGIKUBO', 3);
 
 -- グレード情報の初期データ（一般的なボルダリンググレード）
 INSERT INTO grade_info (gym_id, grade_name, grade_color) VALUES
     (2, '入門', 'DB7093'),  
     (2, '8-6級', 'FF7F00'), 
-    (2, '5級', 'FFFFFF'), 
-    (2, '4級', 'FFFF00'),   
+    (2, '5級', 'FFFFFF'), (2, '4級', 'FFFF00'),   
     (2, '3級', '006400'),
     (2, '2級', 'FF3D3D'),
-    (3, 'VB', '1E90FF'), 
-    (3, 'V0', 'EE82EE'), 
-    (3, 'V1', 'FFFF00'), 
-    (3, 'V2', 'FF0000'),   
-    (3, 'V3', '622D18'),
-    (3, 'V4', 'A0A0A0'), 
-ON CONFLICT (grade_name) DO NOTHING;
+    (3, 'VB', '1E90FF'), (3, 'V0', 'EE82EE'), (3, 'V1', 'FFFF00'), (3, 'V2', 'FF0000'), (3, 'V3', '622D18'), (3, 'V4', 'A0A0A0');
 
 -- トライ結果情報の初期データ
 INSERT INTO result_info (result_name) VALUES
@@ -34,7 +25,7 @@ INSERT INTO result_info (result_name) VALUES
     ('TOP'),      -- 完登
     ('ZONE'),     -- ゾーンまで到達
     ('NOSCORE')   -- スコアなし（トライのみ）
-ON CONFLICT DO NOTHING;
+;
 
 -- エリア情報の初期データ（一般的な壁の種類）
 INSERT INTO area_info (gym_id, area_name) VALUES
@@ -50,8 +41,7 @@ INSERT INTO area_info (gym_id, area_name) VALUES
     (3, '強傾斜'),
     (3, 'シップウォール'),
     (3, 'シップルーフ'),
-ON CONFLICT (area_name) DO NOTHING;
-
+;
 -- 初期データ投入完了メッセージ
 DO $$
 BEGIN
