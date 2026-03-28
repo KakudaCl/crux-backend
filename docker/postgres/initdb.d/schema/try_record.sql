d06d919468cabc02b5f11293e0779a42352acf9e
角田 直樹 <kakuda@iret.co.jp>
2026-03-28 15:59:43 +0900

    feat: トライログテーブルに作成日時と更新日時のカラムを追加

--- docker/postgres/initdb.d/schema/try_record.sql
+++ docker/postgres/initdb.d/schema/try_record.sql
@@ -20,8 +20,23 @@ CREATE TABLE IF NOT EXISTS try_record (
     "try_date" DATE DEFAULT NULL,
     "day_count" INTEGER DEFAULT NULL,
     "monthly_info" VARCHAR(20) DEFAULT NULL,
-    "remarks" VARCHAR(30) DEFAULT NULL
+    "remarks" VARCHAR(30) DEFAULT NULL,
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE OR REPLACE FUNCTION set_try_record_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW."updated_at" := DATE_TRUNC('second', CURRENT_TIMESTAMP);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_set_try_record_updated_at
+BEFORE UPDATE ON try_record
+FOR EACH ROW
+EXECUTE FUNCTION set_try_record_updated_at();
 
 COMMENT ON TABLE try_record IS 'トライ記録';
 COMMENT ON COLUMN try_record."try_id" IS 'トライID';
@@ -34,3 +49,5 @@ COMMENT ON COLUMN try_record."try_date" IS 'トライ日';
 COMMENT ON COLUMN try_record."day_count" IS 'トライ日数';
 COMMENT ON COLUMN try_record."monthly_info" IS 'マンスリー情報';
 COMMENT ON COLUMN try_record."remarks" IS '備考';
+COMMENT ON COLUMN try_record."created_at" IS '作成日時';
+COMMENT ON COLUMN try_record."updated_at" IS '更新日時';
